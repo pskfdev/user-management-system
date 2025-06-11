78f30cc44a110ab88a1991a704b6cbd4e4d74b75
Pongsakan-fluk <pongsakan.obr@futurelifestyle.co.th>
2025-06-11 14:28:40 +0700

    :lipstick: Update profile

--- frontend/src/route/layout/DashboardLayout.tsx
+++ frontend/src/route/layout/DashboardLayout.tsx
@@ -1,8 +1,18 @@
 import Sidebar from "@/components/nav/Sidebar";
-import { CircleUserRound } from "lucide-react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Link, Outlet } from "react-router";
 
 function DashboardLayout() {
+  const username = "Pongsakan";
+
   return (
     <main className="min-h-screen p-5 flex flex-col">
       <div className="flex h-full flex-grow space-x-5">
@@ -10,10 +20,39 @@ function DashboardLayout() {
 
         {/* Content right */}
         <div className="w-full">
-          <div className="flex justify-end py-2 text-slate-700">
-            <CircleUserRound size={32} />
+          {/* Profile */}
+          <div className="flex justify-end py-2 text-black">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar className="cursor-pointer border-2 w-10 h-10 border-indigo-500">
+                  <AvatarFallback className="uppercase bg-indigo-200">
+                    {username && username.slice(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  <h4 className="text-blue-500 text-sm">{username}</h4>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                  <Link to="/users">Home</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link to="/users">user</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="text-red-500 cursor-pointer"
+                  /* onClick={() => {
+                    userLogout();
+                  }} */
+                >
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-          
+
           <hr className="my-5" />
 
           <Outlet />
